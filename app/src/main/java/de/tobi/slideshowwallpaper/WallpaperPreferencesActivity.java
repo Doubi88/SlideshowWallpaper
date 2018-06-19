9a1298757c381e8030e836df7b881406cf77725f
Doubi88 <tobis_mail@yahoo.de>
2018-06-19 10:27:14 +0200

    WallpaperPreferencesActivity extends AppCompatActivity

--- app/src/main/java/de/tobi/slideshowwallpaper/WallpaperPreferencesActivity.java
+++ app/src/main/java/de/tobi/slideshowwallpaper/WallpaperPreferencesActivity.java
@@ -3,8 +3,9 @@ package de.tobi.slideshowwallpaper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class WallpaperPreferencesActivity extends FragmentActivity {
+public class WallpaperPreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
