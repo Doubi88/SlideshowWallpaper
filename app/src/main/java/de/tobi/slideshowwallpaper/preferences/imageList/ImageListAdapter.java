19f8abec92420b8208bf7e3419b4a40b3366eb9d
Doubi88 <tobis_mail@yahoo.de>
2022-08-29 22:09:01 +0200

    Migrate to Android API 28

--- app/src/main/java/de/tobi/slideshowwallpaper/preferences/imageList/ImageListAdapter.java
+++ app/src/main/java/de/tobi/slideshowwallpaper/preferences/imageList/ImageListAdapter.java
@@ -4,7 +4,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
