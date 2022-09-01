ecefe10d9022f79d6d3df9645d395590f50c0c8b
Doubi88 <tobis_mail@yahoo.de>
2022-09-01 13:11:19 +0200

    Added GPLv3 license

--- app/src/main/java/de/tobi/slideshowwallpaper/SlideshowWallpaperService.java
+++ app/src/main/java/de/tobi/slideshowwallpaper/SlideshowWallpaperService.java
@@ -1,3 +1,21 @@
+/*
+ * Slideshow Wallpaper: An Android live wallpaper displaying custom images.
+ * Copyright (C) 2022  Doubi88 <tobis_mail@yahoo.de>
+ *
+ * Slideshow Wallpaper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Slideshow Wallpaper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 package de.tobi.slideshowwallpaper;
 
 import android.content.SharedPreferences;
