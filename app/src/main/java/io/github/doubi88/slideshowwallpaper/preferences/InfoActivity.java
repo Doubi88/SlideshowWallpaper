2a4a5f27d7fa254e95fb136e345e1d4e4c3c517c
Doubi88 <tobis_mail@yahoo.de>
2023-07-26 21:45:03 +0200

    Add Info screen entry in preferences (Not ready)

---
+++ app/src/main/java/io/github/doubi88/slideshowwallpaper/preferences/InfoActivity.java
@@ -0,0 +1,16 @@
+package io.github.doubi88.slideshowwallpaper.preferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.doubi88.slideshowwallpaper.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
